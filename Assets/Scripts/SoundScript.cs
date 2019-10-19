7774e280fb1fab8ef4b6af133be23ddfbd82cae5
mattrossman <mrossman@umass.edu>
2019-10-19 03:59:54 -0400

    Make the fat bird pulse

--- Assets/Scripts/SoundScript.cs
+++ Assets/Scripts/SoundScript.cs
@@ -8,6 +8,9 @@ public class SoundScript : MonoBehaviour
     public string chirpsDir;
     private AudioSource audioSource;
     private AudioClip[] audioClips;
+    private float inflate = 1;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +23,16 @@ public class SoundScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 target = new Vector3(inflate, inflate, inflate);
+        Vector3 diff = target - transform.localScale;
+        transform.localScale += diff / 2;
+        inflate -= (inflate - 1) / 10;
     }
 
     void PlayRandomSound()
     {
         audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
         audioSource.Play();
+        inflate = 2;
     }
 }
