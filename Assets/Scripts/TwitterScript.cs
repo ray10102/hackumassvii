033b5232cfabca17fee3a4e72396512af12fb6eb
mattrossman <mrossman@umass.edu>
2019-10-19 09:24:23 -0400

    Fetch and stream trending topics

--- Assets/Scripts/TwitterScript.cs
+++ Assets/Scripts/TwitterScript.cs
@@ -1,33 +1,87 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Twity.DataModels.Responses;
+using Twity;
+using Twity.DataModels.Core;
+using Twity.DataModels.Trends;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
 
 public class TwitterScript : MonoBehaviour
 {
+    private Stream stream;
+    private Indico indico;
     // Start is called before the first frame update
     void Start()
     {
+        indico = GetComponent<Indico>();
+
         Twity.Oauth.consumerKey = "QWfElQsOE4s7YpcT1q5OS0Dvi";
         Twity.Oauth.consumerSecret = "oerO9Mfr1Verbd64TydNi3LZnIE8PbWXm0iIfPJjgnKlw0HTM5";
         Twity.Oauth.accessToken = "1145564096-gcgjSgGQCDlltojG3cN2l1x4AfnkOubnzzxLjS5";
         Twity.Oauth.accessTokenSecret = "uWrEvMKGsoSVnd5GgAz2ZlkciFShVERjrCbysA5FPMjaC";
-
-        Dictionary<string, string> parameters = new Dictionary<string, string>();
-        parameters["id"] = "1185314716610002944";
-        StartCoroutine(Twity.Client.Get("statuses/show", parameters, Callback));
+    
+        stream = new Stream(StreamType.PublicFilter);
+        StartTrendingTopics();
+        // StartStream(tracks);
     }
 
-    void Callback(bool success, string response)
+    private void StartTrendingTopics()
+    {
+        Dictionary<string, string> parameters = new Dictionary<string, string>();
+        parameters["id"] = "2477058";
+        StartCoroutine(Twity.Client.Get("trends/place", parameters, StartTrendingCallback));
+    }
+
+    void StartTrendingCallback(bool success, string response)
     {
         if (success)
         {
-            SearchTweetsResponse Response = JsonUtility.FromJson<SearchTweetsResponse>(response);
             Debug.Log(response);
+            Regex rgx = new Regex(@"name"":""([^""]*)"",""url");
+            List<string> topics = new List<string>();
+            foreach (Match match in rgx.Matches(response))
+            {
+                topics.Add(match.Groups[1].Value);
+            }
+            StartStream(topics);
         }
         else
         {
             Debug.Log(response);
         }
+    }
+
+    private void StartStream(List<string> tracks)
+    {
+        Dictionary<string, string> streamParameters = new Dictionary<string, string>();
+
+        Twity.FilterTrack filterTrack = new Twity.FilterTrack(tracks);
+        streamParameters.Add(filterTrack.GetKey(), filterTrack.GetValue());
+        StartCoroutine(stream.On(streamParameters, OnStream));
+    }
+
+
+    private void OnStream(string response, StreamMessageType messageType)
+    {
+        try
+        {
+            if (messageType == StreamMessageType.Tweet)
+            {
+                Tweet tweet = JsonUtility.FromJson<Tweet>(response);
+                indico.GetSentiment(tweet.text, (Indico.Sentiment s) => Debug.Log($"{s.result} : {tweet.text}"));
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        stream.Off();
     }
 }
