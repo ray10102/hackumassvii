2145be8925c47909b7c7ec632e26c256bfeda33e
mattrossman <mrossman@umass.edu>
2019-10-19 03:22:34 -0400

    Add script to play random chirp

---
+++ Assets/Scripts/SoundScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+    private AudioSource randomSound;
+
+    public AudioClip[] audioSources;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSound = GetComponent<AudioSource>();
+        InvokeRepeating("PlayRandomSound", 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void PlayRandomSound()
+    {
+        int random = Random.Range(0, audioSources.Length);
+        Debug.Log("Playing clip: " + audioSources[random]);
+        randomSound.clip = audioSources[random];
+        randomSound.Play();
+    }
+}
